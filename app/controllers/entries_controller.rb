fab18a8d106859f65705e70ffb51caed1b4f61ec
Steve Sherk <steven.sherk@kellogg.northwestern.edu>
2024-02-17 04:08:02 +0000

    Likely finished with hw3, pending minor changes

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,16 +5,12 @@ class EntriesController < ApplicationController
   end
 
   def create
-    # start with a new entry
     @entry = Entry.new
-    # assign user-entered form data to entry's columns
     @entry["title"] = params["title"]
     @entry["posted_on"] = params["posted_on"]
     @entry["description"] = params["description"]
     @entry["place_id"] = params["place_id"]
-    # save entry row
     @entry.save
-    # redirect user
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,19 +7,15 @@ class PlacesController < ApplicationController
   end
 
   def create
-    # start with a new Place
     @place = Place.new
-    # assign user-entered form data to place's columns
     @place["name"] = params["name"]
-    # save Place row
     @place.save
-    # redirect user
     redirect_to "/places"
   end
 
   def show
     @place = Place.find_by({"id"=>params["id"]})
-    @entries = Entry.all
+    @entries = Entry.where({"place_id"=>@place["id"]})
   end
 
 end
