3d56345aa0058c56a0f6afdf620095cfaf0434f3
Steve Sherk <steven.sherk@kellogg.northwestern.edu>
2024-02-17 02:45:00 +0000

    working on hw3

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,15 @@
+class EntriesController < ApplicationController
+
+  def show
+    @entries = Entry.find_by({""})
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+end
---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,24 @@
+class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
+
+  def new
+  end
+
+  def create
+    # start with a new Place
+    @place = Place.new
+    # assign user-entered form data to place's columns
+    @place["name"] = params["name"]
+    # save Place row
+    @place.save
+    # redirect user
+    redirect_to "/places"
+  end
+
+  def show
+    @place = Place.find_by({"id"=>params["id"]})
+  end
+
+end
